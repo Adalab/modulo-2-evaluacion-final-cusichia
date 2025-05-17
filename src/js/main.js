df28f1a6e71da0598b1cddb576c2d7233f64b715
Chiara Cusi <cusichia@gmail.com>
2025-05-17 12:57:53 +0200

    Local Storage & empty cart

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,56 @@ const ulProducts = document.querySelector(".js_listProducts");
 const btnFind = document.querySelector(".js_btnFind");
 const inputFind = document.querySelector(".js_inputFind");
 const btnComprar = document.querySelector(".js_btnComprar");
+const ulCart = document.querySelector(".js_listCart");
+const btnEmptyCart = document.querySelector(".js_btnEmptyCart");
 
 // VARIABLES GLOBALES
 let urlProducts =
 	"https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json";
 
 let allProducts = [];
+let cartProducts = [];
 
 // FUNCIONES
-const renderProducts = (allProducts) => {
-	console.log("pintando productos");
+function renderOneProductComprar(oneProduct){
+	let html = `<li class="ulStyle__liStyle">
+              <img class="ulStyle__img" src="${oneProduct.image}" alt="imagen">
+              <h4 class="ulStyle__productName">${oneProduct.title}</h4>
+              <p class="ulStyle__productPrice">${oneProduct.price}€</p>
+              <button class="ulStyle__btnComprar js_btnComprar" id="${oneProduct.id}"> Comprar </button>
+            </li>`;
+	return html;
+}
+
+function renderOneProductEliminar(oneProduct) {
+	let html = `<li class="ulStyle__liStyle">
+              <img class="ulStyle__img" src="${oneProduct.image}" alt="imagen">
+              <h4 class="ulStyle__productName">${oneProduct.title}</h4>
+              <p class="ulStyle__productPrice">${oneProduct.price}€</p>
+              <button class="ulStyle__btnEliminar js_btnComprar" id="${oneProduct.id}"> Eliminar </button>
+            </li>`;
+	return html;
+}
+
+function renderProducts(allProducts) {
     ulProducts.innerHTML = ""; 
 	for (const product of allProducts) {
-		ulProducts.innerHTML += `<li class="liStyle">
-              <img src="https://placecats.com/g/100/150" alt="imagen">
-              <h3 class="productName">${product.title}</h3>
-              <p class="productPrice">${product.price}</p>
-              <button class="btnComprar js_btnComprar"> Comprar </button>
-            </li>`;
+		let foundProducts = cartProducts.find((productCart) => productCart.id === product.id)
+		if (foundProducts != undefined) {
+			ulProducts.innerHTML += renderOneProductEliminar(product);
+		} else {
+			ulProducts.innerHTML += renderOneProductComprar(product);
+		}
 	}
 };
+
+function renderCart() {
+	ulCart.innerHTML = ""; 
+	for (const product of cartProducts) {
+		ulCart.innerHTML += renderOneProductEliminar(product);
+	}
+}
+
 
 // FUNCIONES MANEJADORAS
 function handleClickFind(event) {
@@ -38,22 +68,58 @@ function handleClickFind(event) {
 	renderProducts(filterProducts);
 }
 
-/* function handleClickComprar(event) {
-    console.log("click en comprar");
-} */
+function handleClickComprar(event) {
+	const clickIdComprar = parseInt(event.target.id);
+	
+	if (!clickIdComprar) return; //si no clico encima del boton comprar salgo de la funcion
+
+	let foundProducts = allProducts.find((product) => product.id === clickIdComprar);
+
+	//condición para añadir, que no esté en el array de carrito
+	let existeEnCart = cartProducts.findIndex(product => product.id === clickIdComprar);
+	
+	if (existeEnCart === -1) {
+		cartProducts.push(foundProducts); 
+	} else {
+		cartProducts.splice(existeEnCart, 1);
+	}
+
+	renderCart(cartProducts);
+	renderProducts(allProducts);
+	
+	localStorage.setItem('myCart', JSON.stringify(cartProducts));
+}
+
+function handleClickbtnEmptyCart() {
+	cartProducts = [];
+	ulCart.innerHTML = "";
+	localStorage.removeItem("myCart"); 
+	renderProducts(allProducts);
+}
 
 // LLAMAR FUNCIONES
 
+
 // EVENTOS
 btnFind.addEventListener("click", handleClickFind);
-//btnComprar.addEventListener("click", handleClickComprar);
+ulProducts.addEventListener("click", handleClickComprar);
+ulCart.addEventListener("click", handleClickComprar);
+btnEmptyCart.addEventListener("click", handleClickbtnEmptyCart);
 
 
 // LLAMADAS A LA API
 fetch(urlProducts)
 	.then((response) => response.json())
 	.then((data) => {
-		console.log("hola fetch");
 		allProducts = data;
+
+		localStorage.setItem("Products", JSON.stringify(allProducts)); //guardar todos productos en LS
+
 		renderProducts(allProducts);
 	});
+
+if (localStorage.getItem("myCart") !== null) {
+	cartProducts = JSON.parse(localStorage.getItem("myCart"));
+	renderProducts(allProducts);
+	renderCart();
+}
